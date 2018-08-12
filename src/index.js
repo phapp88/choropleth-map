61baf38740cc0df513e1a7bb57fd5a05128a1b61
Paul <phapp1988@gmail.com>
2018-08-12 12:02:40 -0500

    Implement tooltip

--- src/index.js
+++ src/index.js
@@ -9,35 +9,54 @@ Promise.all([d3.json(dataUrl), d3.json(mapUrl)])
   .then((data) => {
     const [eduData, mapData] = data;
     const eduMap = d3.map();
+    const nameMap = d3.map();
     const path = d3.geoPath();
-    eduData.forEach(d => eduMap.set(d.fips, d.bachelorsOrHigher));
+    eduData.forEach((d) => {
+      eduMap.set(d.fips, d.bachelorsOrHigher);
+      nameMap.set(d.fips, `${d.area_name}, ${d.state}`);
+    });
     const [eduMin, eduMax] = d3.extent(Object.values(eduMap));
     const x = d3.scaleLinear()
       .domain([eduMin, eduMax])
       .rangeRound([600, 900]);
     const color = d3.scaleThreshold()
-      .domain(d3.range(eduMin, eduMax, (eduMax - eduMin) / 9))
+      .domain(d3.range(eduMin, eduMax, (eduMax - eduMin) / 8))
       .range(d3.schemeBlues[9]);
 
     const svg = d3.select('svg');
+    const tooltip = d3.select('main')
+      .append('div').attr('id', 'tooltip').style('opacity', 0);
 
     svg.append('g')
-      .attr('class', 'county')
       .selectAll('path')
       .data(topojson.feature(mapData, mapData.objects.counties).features)
       .enter()
       .append('path')
+      .attr('class', 'county')
       .attr('fill', (d) => {
-        d.bachelorsOrHigher = eduMap.get(d.id);
-        return color(d.bachelorsOrHigher);
+        d.properties = {
+          'data-education': eduMap.get(d.id),
+          'data-name': nameMap.get(d.id),
+        };
+        return color(d.properties['data-education']);
       })
+      .attr('data-fips', d => d.id)
+      .attr('data-education', d => d.properties['data-education'])
       .attr('d', path)
-      .append('title')
-      .text(d => `${d.bachelorsOrHigher}%`);
+      .on('mouseover', (d) => {
+        tooltip.transition().duration(200).style('opacity', 0.9);
+        tooltip.html(`${d.properties['data-name']}: ${d.properties['data-education']}%`)
+          .attr('data-education', d.properties['data-education'])
+          .style('left', `${d3.event.pageX + 12.5}px`)
+          .style('top', `${d3.event.pageY - 20}px`);
+      })
+      .on('mouseout', () => {
+        tooltip.transition().duration(500).style('opacity', 0);
+      });
 
     svg.append('path')
       .datum(topojson.mesh(mapData, mapData.objects.states, (a, b) => a !== b))
-      .attr('class', 'state')
+      .attr('class', 'states')
       .attr('d', path);
 
     const g = svg.append('g')
